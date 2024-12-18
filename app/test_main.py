9244d567c9cf108f3d116341f16c54b6622add3a
IvankaKuzin <kuziniwanka@gmail.com>
2024-12-18 22:47:45 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,40 @@
-# write your code here
+import pytest
+import datetime
+
+from app.main import outdated_products
+
+
+@pytest.fixture()
+def products():
+    return [
+        {
+            "name": "yogurt",
+            "expiration_date": datetime.date.today() - datetime.timedelta(days=1),
+            "price": 160
+        },
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2024, 12, 19),
+            "price": 600
+        },
+        {
+            "name": "orange",
+            "expiration_date": datetime.date.today(),
+            "price": 160
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2024, 12, 20),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2024, 12, 1),
+            "price": 160
+        },
+    ]
+
+
+
+def test_outdated_products(products):
+    assert outdated_products(products) == ["yogurt", "duck"]
